18014068148bf0e8e9a0eb0a280c9647e37ab2cc
Patricia Xiao <patricia.xiao@berkeley.edu>
2019-01-14 14:21:38 -0800

    recorded the documentation

--- setup.sh
+++ setup.sh
@@ -4,15 +4,11 @@ virtualenv -p python3.6 venv
 
 source ./venv/bin/activate
 
+# https://faroit.github.io/keras-docs/1.2.2/
 pip install keras==1.2.1
 pip install theano==1.0.3 # 0.9.0 for python 3.5
 # tensorflow is not working anyway
-# pip install tensorflow==0.11 # 0.11
 pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0-py3-none-any.whl
-# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0rc0-py3-none-any.whl
-# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0rc1-py3-none-any.whl
-# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0rc2-py3-none-any.whl
-# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.12.0-py3-none-any.whl
 
 pip install numpy==1.15.0
 
