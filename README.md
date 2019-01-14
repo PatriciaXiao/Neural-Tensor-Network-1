3507a2b9f8ca10abd7a169a0dc2c39e765d90533
Patricia Xiao <patricia.xiao@berkeley.edu>
2019-01-13 19:26:46 -0800

    updates

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Neural-Tensor-Network
 This is an implementation of Neural Tensor Network
+
+```shell
+jupyter notebook NeuralTensorLayer.ipynb
+```
---
+++ setup.sh
@@ -0,0 +1,21 @@
+# https://www.python.org/downloads/mac-osx/
+
+virtualenv -p python3.6 venv
+
+source ./venv/bin/activate
+
+pip install keras==1.2.1
+pip install theano==1.0.3 # 0.9.0 for python 3.5
+# tensorflow is not working anyway
+# pip install tensorflow==0.11 # 0.11
+pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0-py3-none-any.whl
+# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0rc0-py3-none-any.whl
+# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0rc1-py3-none-any.whl
+# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.11.0rc2-py3-none-any.whl
+# pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.12.0-py3-none-any.whl
+
+pip install numpy
+
+# python train_linkpred.py -d twitter --bases 30 --hidden 16 --l2norm 5e-4 --testing
+
+deactivate
